7c246082ae49c52f3ff000d35657d4af321d2bcd
wsigma21 <wallstlbl@gmail.com>
2024-01-19 13:36:22 +0900

    add: パスパラメータ

--- main.py
+++ main.py
@@ -4,6 +4,14 @@ from fastapi import FastAPI
 app = FastAPI()
 
 # HTTPメソッドのGETで"/"にアクセスがあったら、その下の関数を処理する
-@app.get("/") # パスオペレーション
-async def index():
-  return {"message": "Hello World"}
+# @app.get("/") # パスオペレーション
+# async def index():
+#   return {"message": "Hello World"}
+
+@app.get("/countries/japan") 
+async def country():
+  return {"message": "This is Japan"}
+
+@app.get("/countries/{country_name}") 
+async def country(country_name: str):
+  return {"country_name": country_name}
