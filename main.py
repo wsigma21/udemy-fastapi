11d237ca90b1b75fa017151bd40190c16737d8e5
wsigma21 <wallstlbl@gmail.com>
2024-01-19 13:45:35 +0900

    add: クエリパラメータ

--- main.py
+++ main.py
@@ -8,10 +8,17 @@ app = FastAPI()
 # async def index():
 #   return {"message": "Hello World"}
 
-@app.get("/countries/japan") 
-async def country():
-  return {"message": "This is Japan"}
+# @app.get("/countries/") 
+# async def country(country_name: str = 'Japan', country_no: int = 1):
+#   return {
+#     "country_name": country_name,
+#     "country_no": country_no,
+#   }
 
 @app.get("/countries/{country_name}") 
-async def country(country_name: str):
-  return {"country_name": country_name}
+async def country(country_name: str = 'Japan', city_name: str = 'Tokyo'):
+  return {
+    "country_name": country_name,
+    "city_name": city_name,
+  }
+
